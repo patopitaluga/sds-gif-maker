168a5197f5aac1ac115196566c1c829c21cbd0b0
Patricio Pitaluga <patricio.pitaluga@gmail.com>
2025-07-04 16:33:45 -0300

    Fix

---
+++ local-server.js
@@ -0,0 +1,23 @@
+import http from 'http';
+import { resolve } from 'path';
+import { readFileSync } from 'fs';
+
+const __dirname = resolve(new URL('.', import.meta.url).pathname);
+
+http.createServer((req, res) => {
+  if (req.url === '/gif.js') {
+    res.writeHead(200, { 'Content-Type': 'text/javascript' });
+    res.end(readFileSync(resolve(__dirname, './gif.js'), 'utf8'));
+  }
+  if (req.url === '/gif.worker.js') {
+    res.writeHead(200, { 'Content-Type': 'text/javascript' });
+    res.end(readFileSync(resolve(__dirname, './gif.worker.js'), 'utf8'));
+  }
+  if (req.url === '/style.css') {
+    res.writeHead(200, { 'Content-Type': 'text/css' });
+    res.end(readFileSync(resolve(__dirname, './style.css'), 'utf8'));
+  }
+  if (req.url === '/')
+    res.end(readFileSync(resolve(__dirname, './index.html'), 'utf8'));
+})
+  .listen(3000, () => console.log('Listening port 3000'));
